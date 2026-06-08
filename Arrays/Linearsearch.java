1178c68e6d85895b360f8bf7377970e7e0ec23a6
AHR144 <ahonufrfai144@gmail.com>
2026-06-08 12:45:43 +0600

    Added today's array practice files

---
+++ Arrays/Linearsearch.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.Scanner;
+public class Linearsearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int[] numbers = new int[size];
+               
+        
+        //input
+            for (int i = 0; i < size; i++) {
+                numbers[i] = sc.nextInt();
+            }
+
+        int x = sc.nextInt();     
+         //output
+        for (int i = 0; i <numbers.length; i++) {
+          if(numbers[i] == x){
+              System.out.println("x is present in the array at index " + i);
+          }
+        }
+       sc.close();
+
+    }
+        
+}
---
+++ Arrays/firstarry.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class firstarry {
+    public static void main(String[] args) {
+
+        // int[] marks = new int[3];
+        int[] marks = {97, 98, 95};
+        // marks[0] = 97; //phy
+        // marks[1] = 98; //chem
+        // marks[2] = 95; //eng
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
