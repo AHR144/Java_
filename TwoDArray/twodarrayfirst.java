c46f27b5ae9548563aeaddb72511c5b02acc8b48
AHR144 <ahonufrfai144@gmail.com>
2026-06-09 21:35:03 +0600

    Added 2D array practice files

---
+++ TwoDArray/twodarrayfirst.java
@@ -0,0 +1,35 @@
+package TwoDArray;
+
+import java.util.*;
+
+public class twodarrayfirst {
+
+    public static void main(String[] args) {
+   
+    Scanner sc = new Scanner(System.in);
+
+    int rows = sc.nextInt();
+    int cols = sc.nextInt();
+
+    int[][] numbers = new int[rows][cols];
+
+
+    //input
+    for(int i = 0; i<rows;i++){
+        //cols
+        for (int j = 0; j <cols; j++) {
+            numbers[i][j] = sc.nextInt();           
+        }
+    }
+    sc.close();
+    //output
+   for(int i = 0; i<rows;i++){       
+        for (int j = 0; j <cols; j++) {
+          System.out.println(numbers[i][j]+" ");            
+        }
+        System.out.println();
+    }
+       
+  }
+}
+
